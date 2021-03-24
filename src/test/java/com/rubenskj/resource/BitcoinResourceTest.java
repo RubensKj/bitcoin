4dd5fcf0c8083708be88b0f141970b8d43caabac
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-03-23 21:17:18 -0300

    Creating ResourceTest and removing default content.

---
+++ src/test/java/com/rubenskj/resource/BitcoinResourceTest.java
@@ -0,0 +1,18 @@
+package com.rubenskj.resource;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class BitcoinResourceTest {
+
+    @Test
+    public void shouldReturnStatusCode200() {
+        RestAssured
+                .given()
+                .get("bitcoins")
+                .then()
+                .statusCode(200);
+    }
+}
