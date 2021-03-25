afe297d243c5c474fb6f011e9521345513ef1140
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-03-25 20:46:13 -0300

    Creating a second pattern called Repository, to insert the Orders

---
+++ src/main/java/com/rubenskj/repository/OrdemRepository.java
@@ -0,0 +1,10 @@
+package com.rubenskj.repository;
+
+import com.rubenskj.model.Ordem;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class OrdemRepository implements PanacheRepository<Ordem> {
+}
