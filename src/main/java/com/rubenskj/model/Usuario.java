277bd5b415e3cd870864d59dddc5b93eaca0f029
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-03-25 20:29:12 -0300

    Creating an add user feature

--- src/main/java/com/rubenskj/model/Usuario.java
+++ src/main/java/com/rubenskj/model/Usuario.java
@@ -1,12 +1,14 @@
 package com.rubenskj.model;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Usuario {
+public class Usuario extends PanacheEntityBase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
